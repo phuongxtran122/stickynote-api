bf208106e75ab741af6e2c687b21f2e4ec0dec5e
Phuong Tran <phuongxtran122@gmail.com>
2023-05-24 20:41:39 -0400

    testing counter

--- index.py
+++ index.py
@@ -5,11 +5,15 @@ import check
 
 app = Flask(__name__)
 CORS(app)
-
+number = 0
 @app.route("/")
 def default():
-    
-    return "hi"
+    global number
+    number+=1
+    response = {
+        'userID':number
+    }
+    return jsonify(response)
 
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
